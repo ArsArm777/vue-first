55a12149b725c71b221c76e33277e261eb7dad60
Pilarsen <pilarsen@studio-aw.ru>
2023-05-29 00:45:52 +0300

    add posts, form and post add event

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,110 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="app">
+        <form class="form" @submit.prevent>
+            <h4>Create post</h4>
+            <input
+                    v-bind:value="title"
+                    @input="title = $event.target.value"
+                    class="form__input"
+                    type="text"
+                    placeholder="Title">
+            <input
+                    v-bind:value="description"
+                    @input="description = $event.target.value"
+                    class="form__input"
+                    type="text"
+                    placeholder="description">
+            <button class="form__btn" @click="createPost">Add</button>
+        </form>
+        <div class="post" v-for="post in posts">
+            <div class="post__title">{{ post.title }}</div>
+            <div class="post__description">{{ post.description }}</div>
+        </div>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
+<script>
+export default {
+    data() {
+        return {
+            posts: [
+                {
+                    id: 1,
+                    title: 'JS 1',
+                    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, aspernatur!'
+                },
+                {
+                    id: 2,
+                    title: 'JS 2',
+                    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, aspernatur!'
+                },
+                {
+                    id: 3,
+                    title: 'JS 3',
+                    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, aspernatur!'
+                },
+            ],
+            title: '',
+            description: '',
+        }
+    },
+    methods: {
+        createPost() {
+            const newPost = {
+                id: Date.now(),
+                title: this.title,
+                description: this.description,
+            }
+
+            this.posts.push(newPost);
+            this.title = '';
+            this.description = '';
+        },
+    }
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.app {
+    padding: 20px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.post {
+    border: 1px solid #c3c3c3;
+    padding: 20px;
+    margin-top: 20px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
+.form {
     display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    flex-direction: column;
+}
+
+.form__input {
+    width: 100%;
+    border: 1px solid #c3c3c3;
+    padding: 10px 15px;
+    margin-top: 15px;
+}
+
+.form__btn {
+    align-self: end;
+    background: rebeccapurple;
+    padding: 10px 25px;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+}
+
+.form__btn:hover {
+    cursor: pointer;
+    opacity: 0.8;
 }
 </style>
